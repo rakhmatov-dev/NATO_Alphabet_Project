0dd9d385848147ea1ce480644a8ed6eaeb5843de
rakhmatov93khondamir <khondamir.rakhmatov@gmail.com>
2021-11-16 23:56:40 +0500

    Error handling

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pandas
 
-PHONETIC_ALPHABET_FILE = "nato_phonetic_alphabet.csv"
+PHONETIC_ALPHABET_FILE = "/Volumes/Extreme SSD/Python Bootcamp/Day 26/Mac/NATO Alphabet Project/nato_phonetic_alphabet.csv"
 
 phonetic_alphabet_df = pandas.read_csv(PHONETIC_ALPHABET_FILE)
 phonetic_alphabet_dict = {row.letter: row.code for (index, row) in phonetic_alphabet_df.iterrows()}
 
-users_word = input("Enter a word: ")
-result = [phonetic_alphabet_dict[letter] for letter in users_word.upper()]
-print(result)
+
+def convert_into_nato():
+    users_word = input("Enter a word: ")
+    try:
+        result = [phonetic_alphabet_dict[letter] for letter in users_word.upper()]
+    except KeyError:
+        print("Sorry, only letters in the alphabet, please.")
+        convert_into_nato()
+    else:
+        print(result)
+
+
+convert_into_nato()
